bdaa4d5e573bda99f384be80d1fb405043fdd02b
rupjit choudhury <rupjitchoudhury@gmail.com>
2023-11-08 12:41:05 +0530

    Config change to add account

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,35 @@ module "sandbox1" {
   account_customizations_name = "sandbox1-account-customization"
 }
 
+
+## Sandbox2  account provisioing
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rupjit+sandbox2@amazon.com"
+    AccountName               = "sandbox-aft2"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "rupjit+sandbox@amazon.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+## Account tags to be added here
+  account_tags = {
+    "Application" = "sandbox2"
+    "Env"="POC"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox2-account-customization"
+}
+
