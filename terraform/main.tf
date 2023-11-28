e316a4c071fa02bd33b26510d3c6670998161d1f
rupjit choudhury <rupjitchoudhury@gmail.com>
2023-11-28 20:41:59 +0530

    32213

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,3 +91,35 @@ module "sandbox2" {
   account_customizations_name = "sandbox2-account-customization"
 }
 
+
+## Sandbox3  account provisioing
+module "sandbox3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rupjit+sandbox3@amazon.com"
+    AccountName               = "sandbox-aft3"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "rupjit+sandbox@amazon.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+## Account tags to be added here
+  account_tags = {
+    "Application" = "sandbox2"
+    "Env"="POC"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox3-account-customization"
+}
+
